19b6989cd54c8c3a98a0f6322278da5671e854d0
Beteab Tefera <beteabtefera@yahoo.com>
2024-11-16 21:04:15 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (92.85%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def search(self, nums, target):
-        """
-        Understand: 
-            - oh okay so this is quite easy
-            - Given:
-                - nums in ascending order
-                - return the target index
-        """
-
-        #Brute force
-        if target not in nums:
-            return -1
-        else:
-            return nums.index(target)
+        left = 0
+        right = len(nums)-1
+        
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                right = mid-1
+            else:
+                left = mid+1
+        
+        return -1
