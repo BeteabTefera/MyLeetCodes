437a68cd9cf4cd993a7eb06da1b279750371bab9
Beteab Tefera <beteabtefera@yahoo.com>
2025-01-01 22:16:24 -0600

    Time: 0 ms (100.00%) | Memory: 12.8 MB (8.93%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        U: 
+            - Need to create an array outpu
+            ex: Nums = [1,2,1]
+
+        """
+        return nums * 2
+        
