494a1504ec2df64cce33eec30bc5fe1c2dd11a10
Beteab Tefera <beteabtefera@yahoo.com>
2025-01-22 23:42:21 -0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (32.25%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,12 +1,19 @@
 class Solution(object):
     def removeElement(self, nums, val):
         """
-            U: must use inplace removal, so no creating another array. 
-            M: array problem
-            P: 
-                - plan to count the number of occurence of val element using array.count() and loop for that range and remove the val
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
         """
-        for i in range(nums.count(val)):
-            nums.remove(val)
-        
-        return len(nums)
+        left, right = 0 , len(nums) - 1
+        while left <= right:
+            if nums[left] == val:
+                # Swap with the element at the 'right' pointer
+                nums[left], nums[right] = nums[right], nums[left]
+                # Decrement the 'right' pointer
+                right -= 1
+            else:
+                # Increment the 'left' pointer if the element is valid
+                left += 1
+
+        return left
