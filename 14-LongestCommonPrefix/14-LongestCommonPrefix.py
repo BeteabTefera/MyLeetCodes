8a6a7814401cde81ef8e707293c860be55c830bd
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-18 14:50:58 -0500

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,19 @@
+# Last updated: 8/18/2026, 2:50:57 PM
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:  # Handle empty input list
+            return ""
+        
+        prefix = strs[0]  # Start with the first string as the prefix
+        
+        for s in strs[1:]:
+            # Reduce the prefix until it matches the current string
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]  # Remove the last character from the prefix
+                if not prefix:  # If prefix becomes empty, return
+                    return ""
+        
+        return prefix
+
+            
