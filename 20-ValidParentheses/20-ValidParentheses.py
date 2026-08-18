3668fc9ff7f1fa9354b64b0d290323ba48a91f91
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-18 14:50:56 -0500

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,30 @@
+# Last updated: 8/18/2026, 2:50:55 PM
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        #create a relationship 
+        rel = {'(':')', '{':'}', '[':']'}
+        #base case
+        if len(s) <= 1 or s[0] in  (')', '}', ']') :
+            return False
+        
+        #stack implementation
+        stack = []
+
+        for i in s:
+            if i in ('(', '{', '['):
+                stack.append(i)
+            else:
+                try:
+                    if rel[stack[-1]] == i:
+                        stack.pop()
+                    else:
+                        return False
+                except:
+                    return False
+    
+        return True if not stack else False
+        
