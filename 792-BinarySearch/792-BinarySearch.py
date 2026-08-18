60b7bc431a78fab94020c1446340cb2f42f8ad5f
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-18 14:49:41 -0500

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,22 @@
+# Last updated: 8/18/2026, 2:49:41 PM
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        #low and high defined for indexes
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid -1
+        return -1
