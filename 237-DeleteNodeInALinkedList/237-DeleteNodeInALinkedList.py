56723d8f4c0df5b3a2b97f3c945603a9e67337e4
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-18 14:49:59 -0500

    Create 237-DeleteNodeInALinkedList

---
+++ 237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.py
@@ -0,0 +1,15 @@
+# Last updated: 8/18/2026, 2:49:58 PM
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val=node.next.val
+        node.next=node.next.next
