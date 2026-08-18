ecb108a9c3507884e899e5bff96ecb188557f480
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-18 14:49:39 -0500

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.py
@@ -0,0 +1,15 @@
+# Last updated: 8/18/2026, 2:49:39 PM
+class Solution(object):
+    def calPoints(self, operations):
+        stack = []
+        for op in operations:
+            if op == "+":
+                stack.append(stack[-1] + stack[-2])
+            elif op == "D":
+                stack.append(2 * stack[-1])
+            elif op == "C":
+                stack.pop()
+            else:
+                stack.append(int(op))
+        return sum(stack)
+        
