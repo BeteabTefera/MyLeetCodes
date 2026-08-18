79aedc6c7ef642dcf4b3ac567081d62ae6f89f20
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-18 14:50:14 -0500

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.py
@@ -0,0 +1,21 @@
+# Last updated: 8/18/2026, 2:50:13 PM
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        
+        return dp[-1]
