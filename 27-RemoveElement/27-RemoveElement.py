0fbafb6402bd63e3f5d95e508af13088c12912d6
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-18 14:50:50 -0500

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.py
@@ -0,0 +1,20 @@
+# Last updated: 8/18/2026, 2:50:49 PM
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        left, right = 0 , len(nums) - 1
+        while left <= right:
+            if nums[left] == val:
+                # Swap with the element at the 'right' pointer
+                nums[left], nums[right] = nums[right], nums[left]
+                # Decrement the 'right' pointer
+                right -= 1
+            else:
+                # Increment the 'left' pointer if the element is valid
+                left += 1
+
+        return left
